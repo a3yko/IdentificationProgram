adcef720e27352c65e786db198e524d43b41d5be
Adrian Atanasov <adriansatnanasov@gmail.com>
2021-03-04 10:46:19 -0600

    added to github

---
+++ Identification program/Identification program.cpp
@@ -0,0 +1,695 @@
+// Identification program.cpp : Defines the entry point for the console application.
+
+#include "stdafx.h"
+#include <iostream>
+#include "screen.h"
+
+using namespace std;
+
+/*this is a program about me as in mwahahahahahah*/
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//First Name
+	
+	system("Color 09");
+	gotoxy(33, 10);
+	
+	delay(200);
+	cout << "A";
+	delay(200);
+	cout << "D";
+	delay(200);
+	cout << "R";
+	delay(200);
+	cout << "I";
+	delay(200);
+	cout << "A";
+	delay(200);
+	cout << "N" << endl;
+
+	//Last Name
+	gotoxy(33, 11);
+	system("Color 0E");
+	delay(200);
+	cout << "A";
+	delay(200);
+	cout << "T";
+	delay(200);
+	cout << "A";
+	delay(200);
+	cout << "N";
+	delay(200);
+	cout << "A";
+	delay(200);
+	cout << "S";
+	delay(200);
+	cout << "O";
+	delay(200);
+	cout << "V" << endl;
+
+	//Bio
+	gotoxy(33, 25);
+	system("Color 0A");
+	delay(200);
+	cout << "B";
+	delay(200);
+	cout << "I";
+	delay(200);
+	cout << "O";
+	delay(200);
+	cout << ":";
+
+	gotoxy(33, 26);
+	system("Color 0C");
+	delay(200);
+	cout << "B";
+	delay(200);
+	cout << "o";
+	delay(200);
+	cout << "r";
+	delay(200);
+	cout << "n ";
+
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "n ";
+
+	delay(200);
+	cout << "B";
+	delay(200);
+	cout << "u";
+	delay(200);
+	cout << "l";
+	delay(200);
+	cout << "g";
+	delay(200);
+	cout << "a";
+	delay(200);
+	cout << "r";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "a";
+
+
+
+	gotoxy(33, 27);
+	system("Color 0B");
+	delay(200);
+	cout << "C";
+	delay(200);
+	cout << "o";
+	delay(200);
+	cout << "m";
+	delay(200);
+	cout << "p";
+	delay(200);
+	cout << "u";
+	delay(200);
+	cout << "t";
+	delay(200);
+	cout << "e";
+	delay(200);
+	cout << "r";
+	delay(200);
+	cout << "s ";
+
+	delay(200);
+	cout << "a";
+	delay(200);
+	cout << "r";
+	delay(200);
+	cout << "e ";
+
+	delay(200);
+	cout << "l";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "f";
+	delay(200);
+	cout << "e";
+
+	system("Color 02");
+	gotoxy(33, 28);
+	delay(200);
+	cout << "S";
+	delay(200);
+	cout << "o";
+	delay(200);
+	cout << "c";
+	delay(200);
+	cout << "c";
+	delay(200);
+	cout << "e";
+	delay(200);
+	cout << "r ";
+
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "z ";
+
+	delay(200);
+	cout << "l";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "f";
+	delay(200);
+	cout << "e";
+	
+	system("Color 0C");
+	gotoxy(33, 29);
+	delay(200);
+	cout << "S";
+	delay(200);
+	cout << "l";
+	delay(200);
+	cout << "e";
+	delay(200);
+	cout << "e";
+	delay(200);
+	cout << "p ";
+
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "z ";
+
+	delay(200);
+	cout << "a ";
+	
+	delay(200);
+	cout << "B";
+	delay(200);
+	cout << "e";
+	delay(200);
+	cout << "a";
+	delay(200);
+	cout << "u";
+	delay(200);
+	cout << "t";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "f";
+	delay(200);
+	cout << "u";
+	delay(200);
+	cout << "l ";
+
+	delay(200);
+	cout << "t";
+	delay(200);
+	cout << "h";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "n";
+	delay(200);
+	cout << "g";
+
+	system("Color 0F");
+	gotoxy(33,30);
+	delay(200);
+	cout << "V";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "d";
+	delay(200);
+	cout << "e";
+	delay(200);
+	cout << "o ";
+	
+	delay(200);
+	cout << "g";
+	delay(200);
+	cout << "a";
+	delay(200);
+	cout << "m";
+	delay(200);
+	cout << "e";
+	delay(200);
+	cout << "s ";
+
+	delay(200);
+	cout << "a";
+	delay(200);
+	cout << "r";
+	delay(200);
+	cout << "e ";
+
+	delay(200);
+	cout << "G";
+	delay(200);
+	cout << "o";
+	delay(200);
+	cout << "d";
+	delay(200);
+	cout << "l";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "k";
+	delay(200);
+	cout << "e";
+	
+	system("Color 0A");
+	gotoxy(33, 31);
+	delay(200);
+	cout << "I";
+	delay(200);
+	cout << "c";
+	delay(200);
+	cout << "h ";
+	
+	delay(200);
+	cout << "b";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "n ";
+	
+	delay(200);
+	cout << "1";
+	delay(200);
+	cout << "7 ";
+	
+	delay(200);
+	cout << "J";
+	delay(200);
+	cout << "a";
+	delay(200);
+	cout << "h";
+	delay(200);
+	cout << "r";
+	delay(200);
+	cout << "e ";
+	
+	delay(200);
+	cout << "a";
+	delay(200);
+	cout << "l";
+	delay(200);
+	cout << "t";
+	delay(200);
+	cout << "!";
+	delay(2000);
+
+
+
+
+	
+	//Quote - Soccer is simple but playing simple is hard
+	system("Color 0D");
+	gotoxy(20, 60);
+	
+	delay(200);
+	cout << "S";
+	delay(200);
+	cout << "o";
+	delay(200);
+	cout << "c";
+	delay(200);
+	cout << "c";
+	delay(200);
+	cout << "e";
+	delay(200);
+	cout << "r ";
+
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "s ";
+
+	delay(200);
+	cout << "S";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "m";
+	delay(200);
+	cout << "p";
+	delay(200);
+	cout << "l";
+	delay(200);
+	cout << "e ";
+
+	delay(200);
+	cout << "b"; 
+	delay(200);
+	cout << "u";
+	delay(200);
+	cout << "t ";
+
+	delay(200);
+	cout << "P";
+	delay(200);
+	cout << "l";
+	delay(200);
+	cout << "a";
+	delay(200);
+	cout << "y";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "n";
+	delay(200);
+	cout << "g ";
+
+	delay(200);
+	cout << "S";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "m";
+	delay(200);
+	cout << "p";
+	delay(200);
+	cout << "l";
+	delay(200);
+	cout << "e ";
+
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "s ";
+
+	delay(200);
+	cout << "H";
+	delay(200);
+	cout << "a";
+	delay(200);
+	cout << "r";
+	delay(200);
+	cout << "d ";
+	delay(200);
+	
+	cout << "-";
+	delay(200);
+	cout << "J";
+	delay(200);
+	cout << "o";
+	delay(200);
+	cout << "h";
+	delay(200);
+	cout << "a";
+	delay(200);
+	cout << "n ";
+	
+	delay(200);
+	cout << "C";
+	delay(200);
+	cout << "r";
+	delay(200);
+	cout << "u";
+	delay(200);
+	cout << "y";
+	delay(200);
+	cout << "f";
+	delay(200);
+	cout << "f";
+	delay(2000);
+
+
+	//Hobbies 
+	system("Color 09");
+	gotoxy(33, 90);
+	delay(200);
+	cout << "H";
+	delay(200);
+	cout << "O";
+	delay(200);
+	cout << "B";
+	delay(200);
+	cout << "B";
+	delay(200);
+	cout << "I";
+	delay(200);
+	cout << "E";
+	delay(200);
+	cout << "S";
+	delay(200);
+	cout << ":";
+
+
+	system("Color 08");
+	gotoxy(33, 91);
+	delay(200);
+	cout << "F";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "s";
+	delay(200);
+	cout << "h";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "n";
+	delay(200);
+	cout << "g";
+
+	system("Color 04");
+	gotoxy(33, 92);
+	delay(200);
+	cout << "P";
+	delay(200);
+	cout << "a";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "n";
+	delay(200);
+	cout << "t";
+	delay(200);
+	cout << "b";
+	delay(200);
+	cout << "a";
+	delay(200);
+	cout << "l";
+	delay(200);
+	cout << "l";
+
+	system("Color 07");
+	gotoxy(33, 93);
+	delay(200);
+	cout << "R";
+	delay(200);
+	cout << "e";
+	delay(200);
+	cout << "a";
+	delay(200);
+	cout << "d";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "n";
+	delay(200);
+	cout << "g";
+
+	system("Color 0E");
+	gotoxy(33, 94);
+	delay(200);
+	cout << "S";
+	delay(200);
+	cout << "l";
+	delay(200);
+	cout << "e";
+	delay(200);
+	cout << "e";
+	delay(200);
+	cout << "p";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "n";
+	delay(200);
+	cout << "g";
+	
+	system("Color 0D");
+	gotoxy(33, 95);
+	delay(200);
+	cout << "N";
+	delay(200);
+	cout << "a";
+	delay(200);
+	cout << "p";
+	delay(200);
+	cout << "p";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "n";
+	delay(200);
+	cout << "g";
+	delay(2000);
+
+	// Logo
+	system("Color 0E");
+	gotoxy(20, 120);
+	delay(200); 
+	cout << "                     " << endl;
+	delay(200);
+	cout << "                     ,  "<< endl;
+	delay(200);
+	cout << "                ,.  | \ " << endl;
+	delay(200);
+	cout << "               |: \ ; :\ " << endl;
+	delay(200);
+	cout << "               :' ;\| ::\ ;" << endl;
+	delay(200);
+	cout << "                \ : | `::\ " << endl;
+	delay(200);
+	cout << "                _)  |   `:`. " << endl;
+	delay(200);
+	cout << "              ,' , `.    ;: ;" << endl;
+	delay(200);
+	cout << "            ,' ;:  ;''  ,:: |_ " << endl;
+	delay(200);
+	cout << "           /,   ` .    ;::: |:`-.__ " << endl;
+	delay(200);
+	cout << "        _,' _o\  ,::.`:' ;  ;   . \" " << endl;
+	delay(200);
+	cout << "    _,-'           `:.          ;""\," << endl;
+	delay(200);
+	cout << " ,-'                     ,:         `-;, " << endl;
+	delay(200);
+	cout << " \,                       ;:           ;--._ " << endl;
+	delay(200);
+	cout << "  `.______,-,----._     ,' ;:        ,/ ,  ,` " << endl;
+	delay(200);
+	cout << "         / /,-';'  \     ; `:      ,'/,::.::: " << endl;
+	delay(200);
+	cout << "       ,',;-'-'_,--;    ;   :.   ,',',;:::::: " << endl;
+	delay(200);
+	cout << "      ( /___,-'     `.     ;::,,'o/  ,:::::::" << endl;
+	delay(200);
+	cout << "      `'             )    ;:,'o /  ;  ''- -:: " << endl;
+	delay(200);
+	cout << "                      \__ _,'o ,'         ,:: " << endl;
+	delay(200);
+	cout << "                         ) `--'       ,..:::: " << endl;
+	delay(200);
+	cout << "                         ; `.        ,:::::::" << endl;
+	delay(200);
+	cout << "                          ;  ``::.    :::::::" << endl;
+	delay(2000);
+
+	//End
+	system("Color 0B");
+	gotoxy(33, 170);
+	delay(200);
+	cout << "C";
+	delay(200);
+	cout << "o";
+	delay(200);
+	cout << "n";
+	delay(200);
+	cout << "g";
+	delay(200);
+	cout << "r";
+	delay(200);
+	cout << "a";
+	delay(200);
+	cout << "t";
+	delay(200);
+	cout << "s ";
+	
+	delay(200);
+	cout << "N";
+	delay(200);
+	cout << "o";
+	delay(200);
+	cout << "w ";
+	
+	delay(200);
+	cout << "Y";
+	delay(200);
+	cout << "o";
+	delay(200);
+	cout << "u ";
+
+	delay(200);
+	cout << "K";
+	delay(200);
+	cout << "n";
+	delay(200);
+	cout << "o";
+	delay(200);
+	cout << "w ";
+
+	delay(200);
+	cout << "a ";
+
+	delay(200);
+	cout << "L";
+	delay(200);
+	cout << "i";
+	delay(200);
+	cout << "t";
+	delay(200);
+	cout << "t";
+	delay(200);
+	cout << "l";
+	delay(200);
+	cout << "e ";
+
+	delay(200);
+	cout << "A";
+	delay(200);
+	cout << "b";
+	delay(200);
+	cout << "o";
+	delay(200);
+	cout << "u";
+	delay(200);
+	cout << "t ";
+
+	delay(200);
+	cout << "M";
+	delay(200);
+	cout << "e" << endl;
+	delay(2000);
+
+	//Smile :D
+	system("Color 0C");
+	gotoxy(33, 200);
+	delay(200);
+	cout << "                     " << endl;
+	delay(200);
+
+	cout << "                     ***                  ***" << endl;
+	delay(200);
+
+	cout << "                    *****                *****" << endl;
+	delay(200);
+	cout << "                    *****                *****" << endl;
+	delay(200);
+	cout << "                     ***                  ***" << endl;
+	delay(200);
+	cout << "          ***                                        ***" << endl;
+	delay(200);
+	cout << "           ***                                      ***" << endl;
+	delay(200);
+	cout << "            ***                                    ***" << endl;
+	delay(200);
+	cout << "             ***                                  ***" << endl;
+	delay(200);
+	cout << "               ***                              ***" << endl;
+	delay(200);
+	cout << "                 ***                          ***" << endl;
+	delay(200);
+	cout << "                   ***                      ***" << endl;
+	delay(200);
+	cout << "                      **********************" << endl;
+	delay(200);
+	cout << "                         ****************" << endl;
+
+	system("pause");
+	return 0;
+}
+
